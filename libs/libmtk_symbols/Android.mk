e489ca35b4165c50d4dd565dc8a6ddbc8d700902
vishalk17 <vishalkapadi95@gmail.com>
2018-12-23 19:35:34 +0000

    libmtk_symbols: shim jpeg_std_error_MTK
     wrong way actually, fix if you can else update blobs
    
    conflict,
     dlopen failed: cannot locate symbol "jpeg_std_error_MTK" referenced by "/system/lib/libJpgEncPipe.so

--- libs/libmtk_symbols/Android.mk
+++ libs/libmtk_symbols/Android.mk
@@ -11,6 +11,7 @@ LOCAL_SRC_FILES := \
 	icu55.c \
 	crypto.c \
 	ssl.c \
+        mtk_jpeg.c \
         sensorlistener/ISensorServer.cpp \
         sensorlistener/SensorManager.cpp \
         CameraParameters.cpp
