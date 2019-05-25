35ffff8b8c3ad0ed8a50fbfc47dc578f62d457ef
mhdzumair <mhdzumair@gmail.com>
2019-05-25 16:24:17 +0000

    fixed for Crdroid

--- crdroid_woods.mk
+++ crdroid_woods.mk
@@ -4,7 +4,7 @@
 PRODUCT_RELEASE_NAME := woods
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/motorola/woods/device_woods.mk)
@@ -15,14 +15,14 @@ DEVICE_RESOLUTION := 720x1280
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := woods
-PRODUCT_NAME := aosp_woods
+PRODUCT_NAME := CrDroid_woods
 PRODUCT_BRAND := Motorola
 PRODUCT_MODEL := Moto E4
 PRODUCT_MANUFACTURER := Motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=motorola/woods/woods:7.1.1/NMA26.42-113/133:user/release-keys \
-    PRIVATE_BUILD_DESC="woods_retail-user 7.1.1 NMA26.42-11-3 release-keys" 
+    PRIVATE_BUILD_DESC="woods_retail-user 7.1.1 NMA26.42-11-3 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
